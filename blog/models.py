2cc32ac81bcf83b27e28033ba4656cdfe3d6f279
Ethan Schulz <ethan@eschulz.com>
2015-05-12 11:11:02 -0500

    Added models for simple blog post and image upload

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from tinymce.models import HTMLField
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=80)
+	content = HTMLField()
+
+	# Optional image to the side of the post 
+	image = models.ImageField(null=True, blank=True)
+
+class Image(models.Model):
+	image = models.ImageField()
