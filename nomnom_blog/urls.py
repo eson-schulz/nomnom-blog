60c169a1de9673cb843bc0d61891007267d5fd9a
Ethan Schulz <ethan@eschulz.com>
2015-05-12 10:42:30 -0500

    Updated urls to remove Zinnia and implement tinymce

--- nomnom_blog/urls.py
+++ nomnom_blog/urls.py
@@ -5,12 +5,11 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'nomnom_blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/tools/', include('admin_tools.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
+
+    # WYSIWYG editor
+    (r'^tinymce/', include('tinymce.urls')),
 
     # base url, no pages clicked
     url(r'^', include('blog.urls')),
-	#url(r'^comments/', include('django_comments.urls')),
 ]
