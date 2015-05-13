37bafc645b69a02730369aa085620f32b0d72f90
Ethan Schulz <ethan@eschulz.com>
2015-05-12 22:52:27 -0500

    Changed HTMLField to TextField for properly using tinyMCE

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from tinymce.models import HTMLField
 
 class Post(models.Model):
 	title = models.CharField(max_length=80)
-	content = HTMLField()
+	content = models.TextField()
 
 	# Optional image to the side of the post 
 	image = models.ImageField(null=True, blank=True)
