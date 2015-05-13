e97d612a9ea8a13d5367c6c510fd5fd249dd982d
Ethan Schulz <ethan@eschulz.com>
2015-05-12 22:50:07 -0500

    Added form to render TinyMCE for the admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django import forms
+from tinymce.widgets import TinyMCE
 
-# Register your models here.
+from .models import Post, Image
+
+class PostForm(forms.ModelForm):
+	title = forms.CharField(max_length=80)
+	content = forms.CharField(widget=TinyMCE(attrs={'cols': 200, 'rows': 40}))
+	image = forms.ImageField()
+
+	class Meta:
+		model = Post
+		fields = ('title', 'content', 'image') 
+
+class PostAdmin(admin.ModelAdmin):
+	form = PostForm
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Image)
