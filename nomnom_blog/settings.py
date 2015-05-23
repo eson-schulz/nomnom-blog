bdc8bf6caa4fad47b292ed65128c4724a837ade7
Ethan Schulz <ethan@eschulz.com>
2015-05-22 22:36:38 -0500

    Switched double quotes around static to single. Picky, yes.

--- nomnom_blog/settings.py
+++ nomnom_blog/settings.py
@@ -109,7 +109,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # TinyMCE Settings
