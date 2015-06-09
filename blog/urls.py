6035062330ca2870d356c4cda6ccce00211afd74
Ethan Schulz <ethan@eschulz.com>
2015-06-08 23:31:02 -0500

    Removed links to about and contact page from urls

--- blog/urls.py
+++ blog/urls.py
@@ -5,12 +5,12 @@ urlpatterns = [
 
 	# Base url
 	url(r'^$', views.index, name='index'),
-	# Contact page
-	url(r'^contact/$', views.contact, name='contact'),
+	# Contact page - removed for now
+	#url(r'^contact/$', views.contact, name='contact'),
 	# Main blog page
     url(r'^blog/$', views.blog, name='blog'),
-    # Static about page
-    url(r'^about/$', views.about, name='about'),
+    # Static about page - removed for now
+    #url(r'^about/$', views.about, name='about'),
     # Specific post
     url(r'^blog/post/(?P<post_slug>[\w\-]+)/$', views.post, name='post'),
 ]
